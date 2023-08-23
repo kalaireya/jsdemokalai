4c299130552405340973e176fc9572b5c775b8aa
kalaireya <kalaiselvi1309.d@gmail.com>
2023-08-23 16:32:22 +0530

    anu commit

---
+++ dom_concepjs.js
@@ -0,0 +1,5 @@
+//how to get data from form by using js
+ let fname=document.addEventListener('fname').value;
+console.log(fname);
+let lname=document.getElementById('lname').value;
+console.log(lname);
